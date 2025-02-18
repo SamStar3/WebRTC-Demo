d355a3908420e9fe919cb7ff73bc92531c6bb61c
SamStar3 <samstar1044948@gmail.com>
2025-02-18 21:27:56 +0530

    file added

---
+++ changeVideoSize.js
@@ -0,0 +1,25 @@
+
+const supportedConstraints = navigator.mediaDevices.getSupportedConstraints();
+console.log(supportedConstraints);
+
+const changeVideoSize = () => {
+
+    stream.getVideoTracks().forEach(track => {
+
+        const capbilities = track.getCapabilities();
+        const height = document.getElementById('#vid-height').value;
+        const width = document.getElementById('#vid-width').value;
+        const vConstraints = {
+            height: {exact: height < capbilities.height.max? height : capbilities.height.max},
+            width: {exact: width < capbilities.width.max? width : capbilities.width.max},
+            // frameRate: 5,
+            aspectRatio: 10,
+        }
+        track.applyConstraints(vConstraints);
+    })
+        
+    // stream.getTracks().forEach(track=> {
+    //     const capbilities = track.getCapbilities();
+    //     console.log(capbilities);
+    // })
+}
---
+++ scripts.js
@@ -0,0 +1,51 @@
+
+const videoEl = document.querySelector('#my-video');
+let stream = null; //Init stream variable so we can use anywhere
+
+const constraints = {
+    audio: true,    //Use your Headphone or be prepared for feedback!
+    video: true,
+};
+
+const getMicAndCamera = async (e) => {
+
+    try {
+        stream = await navigator.mediaDevices.getUserMedia(constraints);
+        console.log(stream);
+        changeButtons([
+            'green','blue','blue', 'grey', 'grey', 'grey', 'grey', 'grey'
+        ]);
+    } catch (error) {
+        console.log("user denied access to constraints, error: " + error);
+    }
+}
+
+const showMyFeed = e=> {
+    console.log("show my feed is working")
+    if(!stream){
+        alert("stream is still loading...please wait");
+        return;
+    }
+    videoEl.srcObject = stream; // this will show our MediaStream(stream) to our video
+    const tracks = stream.getTracks(); // get all tracks from the stream
+    console.log(tracks); // log all tracks
+    changeButtons([
+        'green','green','blue', 'blue', 'blue', 'grey', 'grey', 'blue'
+    ]);
+}
+
+const stopMyFeed = e=> {
+    if(!stream){
+        alert("stream is still loading...please wait");
+        return;
+    }
+    const tracks = stream.getTracks();
+    tracks.forEach(track => track.stop()); // stop all tracks
+    changeButtons([
+        'blue','grey','grey', 'grey', 'grey', 'grey', 'grey', 'grey '
+    ]);
+}
+
+document.querySelector('#share').addEventListener('click', e=>getMicAndCamera(e));
+document.querySelector('#show-video').addEventListener('click', e=>showMyFeed(e));
+document.querySelector('#stop-video').addEventListener('click', e=>stopMyFeed(e));
